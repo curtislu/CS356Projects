0eb94449c9c4cbe07d59b6fe0530d0011209923a
DannyHo89 <cloud.of.heaven@gmail.com>
2014-02-17 01:25:57 -0800

    Revision 3
    Still need to add textfield for output, checkbox actionlisteners, and
    link GUI to command prompt.

--- RoadRunnerGUI.java
+++ RoadRunnerGUI.java
@@ -10,19 +10,19 @@ import javax.swing.filechooser.FileFilter;
 class RoadRunnerGUI implements ActionListener {
 
 	GridBagConstraints gbc = new GridBagConstraints();
-	JButton jbtnOut, jbtnRun, jbtnSelect;
-	JCheckBox[] jcb = new JCheckBox[17];
+	JButton jbtnOut, jbtnReset, jbtnRun, jbtnSelect;
+	JCheckBox[] jcb = new JCheckBox[11];
 	JFileChooser jfc = new JFileChooser();
-	JFrame jfrm = new JFrame("RoadRunner Dynamic Analysis Framework");
+	JFrame jfrm = new JFrame("RoadRunner Dynamic Analysis Framework - GUI");
 	JLabel jlab = new JLabel("No file selected.");
 	JMenuBar jmb = new JMenuBar();
 	JMenuItem jmiRun, jmiOut;
-	JPanel jpnl1, jpnl2, jpnl3, jpnl4;
+	JPanel jpnl1, jpnl2, jpnl3, jpnlBot, jpnlTop;
 
 	RoadRunnerGUI() {
 		// Frame configurations
-		jfrm.setSize(500, 500);
-		jfrm.getContentPane().setLayout(new GridLayout(2, 2));
+		jfrm.setSize(750, 500);
+		jfrm.getContentPane().setLayout(new GridLayout(2, 1));
 		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jfrm.setLocationRelativeTo(null);
 		jfrm.setVisible(true);
@@ -51,29 +51,31 @@ class RoadRunnerGUI implements ActionListener {
 		JMenu jmPref = new JMenu("Preferences");
 		// Submenu - Tools
 		JMenu jmTool = new JMenu("Tools");
-		JMenuItem tool1 = new JMenuItem("Tool 1");
-		JMenuItem tool2 = new JMenuItem("Tool 2");
-		JMenuItem tool3 = new JMenuItem("Tool 3");
-		JMenuItem tool4 = new JMenuItem("Tool 4");
-		JMenuItem tool5 = new JMenuItem("Tool 5");
+		JMenuItem tool1 = new JMenuItem("Atomizer");
+		JMenuItem tool2 = new JMenuItem("BarrierEraser");
+		JMenuItem tool3 = new JMenuItem("FastTrack");
+		JMenuItem tool4 = new JMenuItem("HappensBefore");
+		JMenuItem tool5 = new JMenuItem("LockSet");
+		JMenuItem tool6 = new JMenuItem("ProtectingLock");
+		JMenuItem tool7 = new JMenuItem("ReadShared");
 		jmTool.add(tool1);
 		jmTool.add(tool2);
 		jmTool.add(tool3);
 		jmTool.add(tool4);
 		jmTool.add(tool5);
+		jmTool.add(tool6);
+		jmTool.add(tool7);
 		// Submenu - Options
 		JMenu jmOpt = new JMenu("Options");
-		JMenuItem opt1 = new JMenuItem("Option 1");
-		JMenuItem opt2 = new JMenuItem("Option 2");
-		JMenuItem opt3 = new JMenuItem("Option 3");
-		JMenuItem opt4 = new JMenuItem("Option 4");
-		JMenuItem opt5 = new JMenuItem("Option 5");
+		JMenuItem opt1 = new JMenuItem("Array");
+		JMenuItem opt2 = new JMenuItem("Classpath");
+		JMenuItem opt3 = new JMenuItem("MaxTID");
+		JMenuItem opt4 = new JMenuItem("NoBarrier");
 		jmOpt.add(opt1);
 		jmOpt.add(opt2);
 		jmOpt.add(opt3);
 		jmOpt.add(opt4);
-		jmOpt.add(opt5);
-		JMenuItem jmiReset = new JMenuItem("Reset", KeyEvent.VK_R);
+		JMenuItem jmiReset = new JMenuItem("Reset GUI", KeyEvent.VK_R);
 		jmiReset.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.ALT_MASK));
 		jmPref.add(jmTool);
 		jmPref.add(jmOpt);
@@ -83,10 +85,13 @@ class RoadRunnerGUI implements ActionListener {
 
 		// Menubar - Help
 		JMenu jmHelp = new JMenu("Help");
+		JMenuItem jmiGet = new JMenuItem("Getting Started", KeyEvent.VK_G);
+		jmiGet.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G, InputEvent.ALT_MASK));
 		JMenuItem jmiDoc = new JMenuItem("Documentation", KeyEvent.VK_D);
 		jmiDoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.ALT_MASK));
 		JMenuItem jmiAbout = new JMenuItem("About", KeyEvent.VK_A);
 		jmiAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.ALT_MASK));
+		jmHelp.add(jmiGet);
 		jmHelp.add(jmiDoc);
 		jmHelp.add(jmiAbout);
 		jmb.add(jmHelp);
@@ -96,13 +101,19 @@ class RoadRunnerGUI implements ActionListener {
 		jmiOut.addActionListener(this);
 		jmiExit.addActionListener(this);
 		jmiReset.addActionListener(this);
+		jmiGet.addActionListener(this);
 		jmiDoc.addActionListener(this);
 		jmiAbout.addActionListener(this);
 
+		// Top panel
+		jpnlTop = new JPanel();
+		jpnlTop.setLayout(new GridLayout(1, 3));
+		jpnlTop.setOpaque(true);
+		
 		// Tool grid
 		jpnl1 = new JPanel();
 		jpnl1.setLayout(new GridLayout(4, 2));
-		jpnl1.setBorder(BorderFactory.createTitledBorder("Tools"));
+		jpnl1.setBorder(BorderFactory.createTitledBorder("Step 1"));
 		jpnl1.setOpaque(true);
 		jcb[0] = new JCheckBox("Atomizer");
 		jcb[1] = new JCheckBox("BarrierEraser");
@@ -118,63 +129,65 @@ class RoadRunnerGUI implements ActionListener {
 
 		// Option grid
 		jpnl2 = new JPanel();
-		jpnl2.setLayout(new GridLayout(5, 2));
-		jpnl2.setBorder(BorderFactory.createTitledBorder("Options"));
+		jpnl2.setLayout(new GridLayout(2, 2));
+		jpnl2.setBorder(BorderFactory.createTitledBorder("Step 2"));
 		jpnl2.setOpaque(true);
-		jcb[7] = new JCheckBox("Option 1");
-		jcb[8] = new JCheckBox("Option 2");
-		jcb[9] = new JCheckBox("Option 3");
-		jcb[10] = new JCheckBox("Option 4");
-		jcb[11] = new JCheckBox("Option 5");
-		jcb[12] = new JCheckBox("Option 6");
-		jcb[13] = new JCheckBox("Option 7");
-		jcb[14] = new JCheckBox("Option 8");
-		jcb[15] = new JCheckBox("Option 9");
-		jcb[16] = new JCheckBox("Option 10");
-		for (int i = 7; i < 17; i++) {
+		jcb[7] = new JCheckBox("Array");
+		jcb[8] = new JCheckBox("Classpath");
+		jcb[9] = new JCheckBox("MaxTID");
+		jcb[10] = new JCheckBox("NoBarrier");
+		for (int i = 7; i < 11; i++) {
 			jcb[i].addActionListener(this);
 			jpnl2.add(jcb[i]);
 		}
 
-		// Java file grid
+		// Button grid
 		jpnl3 = new JPanel();
 		jpnl3.setLayout(new GridBagLayout());
-		jpnl3.setBorder(BorderFactory.createTitledBorder("Java File"));
+		jpnl3.setBorder(BorderFactory.createTitledBorder("Step 3"));
 		jpnl3.setOpaque(true);
 		jbtnSelect = new JButton("Select Java File...");
 		jbtnSelect.addActionListener(this);
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-		gbc.insets = new Insets(0, 0, 40, 0);
+		gbc.insets = new Insets(0, 0, 0, 0);
 		jpnl3.add(jbtnSelect, gbc);
 		gbc.gridx = 0;
 		gbc.gridy = 1;
+		gbc.insets = new Insets(10, 0, 0, 0);
 		jpnl3.add(jlab, gbc);
-
-		// Button grid
-		jpnl4 = new JPanel();
-		jpnl4.setLayout(new GridBagLayout());
-		jpnl4.setBorder(BorderFactory.createTitledBorder("Execute"));
-		jpnl4.setOpaque(true);
 		jbtnRun = new JButton("Run Program");
 		jbtnRun.setEnabled(false);
 		jbtnRun.addActionListener(this);
 		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.insets = new Insets(0, 0, 40, 0);
-		jpnl4.add(jbtnRun, gbc);
+		gbc.gridy = 2;
+		gbc.insets = new Insets(20, 0, 5, 0);
+		jpnl3.add(jbtnRun, gbc);
 		jbtnOut = new JButton("Output File");
 		jbtnOut.setEnabled(false);
 		jbtnOut.addActionListener(this);
 		gbc.gridx = 0;
-		gbc.gridy = 1;
-		jpnl4.add(jbtnOut, gbc);
+		gbc.gridy = 3;
+		gbc.insets = new Insets(5, 0, 20, 0);
+		jpnl3.add(jbtnOut, gbc);
+		jbtnReset = new JButton("Reset GUI");
+		jbtnReset.setEnabled(true);
+		jbtnReset.addActionListener(this);
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		gbc.insets = new Insets(0, 0, 0, 0);
+		jpnl3.add(jbtnReset, gbc);
+		
+		// Bottom panel
+		jpnlBot = new JPanel();
+		
+		jpnlTop.add(jpnl1);
+		jpnlTop.add(jpnl2);
+		jpnlTop.add(jpnl3);
 
 		jfrm.setJMenuBar(jmb);
-		jfrm.getContentPane().add(jpnl1);
-		jfrm.getContentPane().add(jpnl2);
-		jfrm.getContentPane().add(jpnl3);
-		jfrm.getContentPane().add(jpnl4);
+		jfrm.getContentPane().add(jpnlTop);
+		jfrm.getContentPane().add(jpnlBot);
 	}
 
 	public void actionPerformed(ActionEvent ae) {
@@ -198,8 +211,8 @@ class RoadRunnerGUI implements ActionListener {
 			System.exit(0);
 		}
 
-		if (comStr.equals("Reset")) {
-			for (int i = 0; i < 17; i++) {
+		if (comStr.equals("Reset GUI")) {
+			for (int i = 0; i < 11; i++) {
 				jcb[i].setSelected(false);
 			}
 			
@@ -208,6 +221,21 @@ class RoadRunnerGUI implements ActionListener {
 			jbtnRun.setEnabled(false);
 			jmiOut.setEnabled(false);
 			jbtnOut.setEnabled(false);
+		}
+		
+		if (comStr.equals("Getting Started")) {
+			JOptionPane.showMessageDialog(jfrm, "To use this GUI, simply follow the steps below: "
+					+ "\n\n\n"
+					+ "Step 1: Choose the desired tools."
+					+ "\n\n"
+					+ "Step 2: Choose the desired options."
+					+ "\n\n"
+					+ "Step 3a: Choose which java file to run."
+					+ "\n\n"
+					+ "Step 3b: Press \"Run Program\" to run the program."
+					+ "\n\n"
+					+ "Step 4: Press \"Output File\" to show the results in XML format.",
+					"How to - RoadRunnerGUI", JOptionPane.INFORMATION_MESSAGE);
 		}
 
 		if (comStr.equals("Documentation")) {
